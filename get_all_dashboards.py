68648e4dbf960f7730f9b123ee101ccc0d0474e2
Heku Watada <heku.watada@gmail.com>
2022-11-01 08:56:33 +0000

    Added type for dashboards returned from Looker SDK

--- get_all_dashboards.py
+++ get_all_dashboards.py
@@ -5,20 +5,21 @@ Usage:
     python <this file>
 """
 import looker_sdk
+from looker_sdk import models40
 
 # configure the SDK with Looker credentials
 # NOTE: run `make setup` to create `looker.ini` file
 sdk = looker_sdk.init40("./looker.ini")
 
 
-def format_dashboard(dashboard) -> str:
+def format_dashboard(dashboard: models40.DashboardBase) -> str:
     return f"{dashboard.id},{dashboard.title}"
 
 
-def print_dashboard_info_for_all_dashboards():
+def print_dashboard_info_for_all_dashboards() -> None:
     # call Looker SDK to get all dashboards
-    # returns list of DashboardBase objects
-    dashboards = sdk.all_dashboards()
+    # https://developers.looker.com/api/explorer/4.0/methods/Dashboard/all_dashboards?sdk=py
+    dashboards: Sequence[models40.DashboardBase] = sdk.all_dashboards()
 
     # format each dashboard object as string
     dashboards_string = list(map(format_dashboard, dashboards))
